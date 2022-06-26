e049bfd511a75965ab86f698c68b666411a4f1b3
Jason <jkuffler@catalyte.io>
2022-06-25 20:42:37 -0400

    created db.json to act as basic business/id endpoint

--- src/index.js
+++ src/index.js
@@ -2,3 +2,80 @@
 Each page's scripts are separated by: ================================ 
 */ 
 
+const containers = document.querySelectorAll('.container')
+
+//make each shop container clickable
+const shopsContainer = () => {
+    containers.forEach(container => {
+    container.addEventListener('click', (e) => {
+        e.preventDefault();
+        const currentShop = document.querySelector(e.this);
+        container.appendChild(currentShop);
+        }
+      )
+    }
+  )
+};
+
+//call to API and bring in the json as JS objeect and run renderShop
+const getShops = () => {
+  fetch(`https://api.yelp.com/v3/businesses/${id}`)
+  .then(resp => resp.json())
+  .then((shopData) => {
+      shopData.forEach((shop) => renderShop(shop))
+    }
+  )
+};
+
+  //lines 16-29 can be reused in a react component along with virtually all UI elements/behaviors
+const renderShop = (shop) => {
+  const shopContainer = document.querySelector('#shop')
+  const shopSpan = document.createElement('span') 
+  shopSpan.name = shop.name;
+  shopSpan.img_url = shop.img_url;
+  shopSpan.url = shop.url;
+  shopSpan.phone = shop.phone,
+  shopSpan.display_phone = shop.display_phone,
+  shopSpan.review_count = shop.review_count
+  return shopContainer;
+  
+}
+
+  const shopImage = document.createElement('img')
+  shopImage.classList = ''
+  shopImage.src = `.//${shop.name}.jpg`
+  shopSpan.append(shopImage)
+
+  shopsContainer.append(shopSpan)
+
+
+'use strict';
+
+import { client as _client } from 'yelp-fusion'
+
+// Place holder for Yelp Fusion's key
+const apiKey = "MG_mNqNJ2ghKp_Uwlr_HzVOI8eB9KTm2VdjNjgDCVX_TMicJcqS9EcF1FXR4we_fGh2Dbfnkt6-Cl9cY7uPbzflvHlbrK4a0RRTbMHJiRa79mravQjbkKHjdsl63YnYx";
+
+const searchRequest = {
+  "term": 'Four Barrel Coffee',
+  "location": 'san francisco, ca'
+};
+
+const client = _client(apiKey);
+
+client.search(searchRequest).then(response => {
+  const firstResult = response.jsonBody.businesses[0];
+  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(prettyJson);
+}).catch(e => {
+  console.log(e);
+});
+
+
+/* Setup the script to run now */
+function init() {
+  getShops();
+  shopsContainer();
+}
+
+init();
