7774b0e6f20f3f9c5fb527be09fbc48fa3e56989
Jason <jkuffler@catalyte.io>
2022-06-25 20:27:40 -0400

    added shop methods to test in browser and postman

--- src/index.js
+++ src/index.js
@@ -1,31 +1,83 @@
 /*
-Each page's scripts are separated by: ================================ 
-*/ 
+Each page's scripts and their custom-methods/functions are separated by comments
+Pinboard includes docs and misc ctl+click or cpy+pst =  https =//pinboard.opera.com/view/06501e26-cfcb-4eb8-8db6-957a2f2d1dab  
+*/
+/* SHOPS CONTAINER */
 
-/* Pinboard includes docs and misc https://pinboard.opera.com/view/06501e26-cfcb-4eb8-8db6-957a2f2d1dab  */
+const containers = document.querySelectorAll('.container')
 
+//make each shop container clickable
+const shopsContainer = () => {
+    containers.forEach(container => {
+    container.addEventListener('click', (e) => {
+        e.preventDefault();
+        const currentShop = document.querySelector(e.this);
+        container.appendChild(currentShop);
+        }
+      )
+    }
+  )
+};
+
+//call to API and bring in the json as JS objeect and run renderShop
 const getShops = () => {
-  fetch('http://')
+  fetch(`https://api.yelp.com/v3/businesses/${id}`)
   .then(resp => resp.json())
   .then((shopData) => {
       shopData.forEach((shop) => renderShop(shop))
-  })
-}
+    }
+  )
+};
 
-
+  //lines 16-29 can be reused in a react component along with virtually all UI elements/behaviors
 const renderShop = (shop) => {
-  //create span, add class, id, and draggable attribute
-  //this can be reused in a react component along with other UI elements/behaviors
   const shopContainer = document.querySelector('#shop')
+  const shopSpan = document.createElement('span') 
+  shopSpan.name = shop.name;
+  shopSpan.img_url = shop.img_url;
+  shopSpan.url = shop.url;
+  shopSpan.phone = shop.phone,
+  shopSpan.display_phone = shop.display_phone,
+  shopSpan.review_count = shop.review_count
+  return shopContainer;
   
-  const shopSpan = document.createElement('span')
-  shopSpan.id = shop.name
-
+}
 
   const shopImage = document.createElement('img')
   shopImage.classList = ''
   shopImage.src = `.//${shop.name}.jpg`
   shopSpan.append(shopImage)
 
-  shopContainer.append(shopSpan)
+  shopsContainer.append(shopSpan)
+
+
+'use strict';
+
+import { client as _client } from 'yelp-fusion'
+
+// Place holder for Yelp Fusion's key
+const apiKey = 'MG_mNqNJ2ghKp_Uwlr_HzVOI8eB9KTm2VdjNjgDCVX_TMicJcqS9EcF1FXR4we_fGh2Dbfnkt6-Cl9cY7uPbzflvHlbrK4a0RRTbMHJiRa79mravQjbkKHjdsl63YnYx';
+
+const searchRequest = {
+  "term": 'Four Barrel Coffee',
+  "location": 'san francisco, ca'
+};
+
+const client = _client(apiKey);
+
+client.search(searchRequest).then(response => {
+  const firstResult = response.jsonBody.businesses[0];
+  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(prettyJson);
+}).catch(e => {
+  console.log(e);
+});
+
+
+/* Setup the script to run now */
+function init() {
+  getShops();
+  shopsContainer();
 }
+
+init();
